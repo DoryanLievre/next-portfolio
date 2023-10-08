e6d07bf747a1eca8407ac231cbd8ce459f363e5e
Doryan L <dlievre.dev@gmail.com>
2023-10-08 14:55:04 +0200

    feat:create intro section

---
+++ components/intro.js
@@ -0,0 +1,23 @@
+'use client'
+import {useEffect, useState} from "react";
+
+export default function Intro() {
+    const [classes, setClasses] = useState('');
+
+    useEffect(() => {
+        setClasses('visible')
+
+    }, []);
+
+    return (
+        <section id="intro" className={classes}>
+            <div>
+                <p>I'm <span>Doryan Lièvre</span><br/>Full-stack javascript developer,<br/> curious and code lover.</p>
+                <a href="">Contact me</a>
+            </div>
+            <div>
+                <img src="img/me_grayscale.png" alt="Profil picture of me (Doryan Lièvre)" />
+            </div>
+        </section>
+    )
+}
