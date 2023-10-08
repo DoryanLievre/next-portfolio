1d0d81d8ef5827b018b8b731ea4c50239de26edc
Doryan L <dlievre.dev@gmail.com>
2023-10-08 23:07:43 +0200

    feat(Portfolio.js): add project stack details on hover

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -4,12 +4,66 @@ export default function Portfolio() {
       <div className="center">
         <h2>Portfolio</h2>
         <div className="portfolio-container">
-            <a href=''><img src='/images/placeholder-image.jpg' alt=''/><span>Projet 1</span></a>
-            <a href=''><img src='/images/placeholder-image2.jpg' alt=''/><span>Projet 2</span></a>
-            <a href=''><img src='/images/placeholder-image3.jpg' alt=''/><span>Projet 3</span></a>
-            <a href=''><img src='/images/placeholder-image3.jpg' alt=''/><span>Projet 4</span></a>
-            <a href=''><img src='/images/placeholder-image.jpg' alt=''/><span>Projet 5</span></a>
-            <a href=''><img src='/images/placeholder-image2.jpg' alt=''/><span>Projet 6</span></a>
+          <a href="">
+            <img src="/images/placeholder-image.jpg" alt="" />
+            <span>Projet 1</span>
+            <ul>
+                <li>HTML & CSS</li>
+                <li>React</li>
+                <li>Node JS</li>
+                <li>Mongo db</li>
+            </ul>
+          </a>
+          <a href="">
+            <img src="/images/placeholder-image2.jpg" alt="" />
+            <span>Projet 2</span>
+            <ul>
+              <li>HTML & CSS</li>
+              <li>React</li>
+              <li>Node JS</li>
+              <li>Mongo db</li>
+            </ul>
+          </a>
+          <a href="">
+            <img src="/images/placeholder-image3.jpg" alt="" />
+            <span>Projet 3</span>
+            <ul>
+              <li>HTML & CSS</li>
+              <li>React</li>
+              <li>Node JS</li>
+              <li>Mongo db</li>
+            </ul>
+          </a>
+          <a href="">
+            <img src="/images/placeholder-image3.jpg" alt="" />
+            <span>Projet 4</span>
+            <ul>
+              <li>HTML & CSS</li>
+              <li>React</li>
+              <li>Node JS</li>
+              <li>Mongo db</li>
+            </ul>
+          </a>
+          <a href="">
+            <img src="/images/placeholder-image.jpg" alt="" />
+            <span>Projet 5</span>
+            <ul>
+              <li>HTML & CSS</li>
+              <li>React</li>
+              <li>Node JS</li>
+              <li>Mongo db</li>
+            </ul>
+          </a>
+          <a href="">
+            <img src="/images/placeholder-image2.jpg" alt="" />
+            <span>Projet 6</span>
+            <ul>
+              <li>HTML & CSS</li>
+              <li>React</li>
+              <li>Node JS</li>
+              <li>Mongo db</li>
+            </ul>
+          </a>
         </div>
       </div>
     </section>
