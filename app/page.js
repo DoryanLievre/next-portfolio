849eac706452e1b6acd24b53b55d1f6768faec0b
Doryan L <dlievre.dev@gmail.com>
2023-10-08 15:34:35 +0200

    feat:create components Skills.js

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Styles from './styles/home.scss'
 import Header from "@/components/Header";
 import Intro from "@/components/Intro";
 import About from "@/components/About";
+import Skills from "@/components/Skills";
 
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
        <Header />
         <Intro />
         <About />
+        <Skills />
     </main>
   )
 }
---
+++ components/Skills.js
@@ -0,0 +1,45 @@
+export default function Skills() {
+    return (
+        <section id="skills">
+            <div className="center">
+                <h2>Skills</h2>
+                <div className="skills">
+                    <div className="skill">
+                        <img src="" alt="" />
+                        <span>HTML</span>
+                    </div>
+                    <div className="skill">
+                        <img src="" alt="" />
+                        <span>HTML</span>
+                    </div>
+                    <div className="skill">
+                        <img src="" alt="" />
+                        <span>HTML</span>
+                    </div>
+                    <div className="skill">
+                        <img src="" alt="" />
+                        <span>HTML</span>
+                    </div>
+                    <div className="skill">
+                        <img src="" alt="" />
+                        <span>HTML</span>
+                    </div>
+                    <div className="skill">
+                        <img src="" alt="" />
+                        <span>HTML</span>
+                    </div>
+                    <div className="skill">
+                        <img src="" alt="" />
+                        <span>HTML</span>
+                    </div>
+                    <div className="skill">
+                        <img src="" alt="" />
+                        <span>HTML</span>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+    )
+}
