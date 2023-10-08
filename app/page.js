bcbff1080b32f28c5b91bc990db37e1c5378fbfd
Doryan L <dlievre.dev@gmail.com>
2023-10-08 17:37:45 +0200

    feat(sections):add some transition and animation

--- app/page.js
+++ app/page.js
@@ -1,10 +1,23 @@
+'use client';
 import Styles from './styles/home.scss'
 import Header from "@/components/Header";
 import Intro from "@/components/Intro";
 import About from "@/components/About";
 import Skills from "@/components/Skills";
+import {useEffect} from "react";
 
 export default function Home() {
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries =>
+            entries.forEach(entry => {
+                entry.target.classList.toggle("is-visible", entry.isIntersecting);
+            })
+        );
+        observer.observe(document.querySelector("div.skills-container"));
+        document.querySelectorAll("section").forEach(section => {
+            observer.observe(section);
+        });
+    }, []);
   return (
     <main>
        <Header />
