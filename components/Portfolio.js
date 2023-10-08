164ab477a5fb9c4e1e4bebf475d37f860a4fab05
Doryan L <dlievre.dev@gmail.com>
2023-10-08 17:58:38 +0200

    feat(component): add portfolio component

---
+++ components/Portfolio.js
@@ -0,0 +1,9 @@
+export default function Portfolio() {
+    return (
+        <section id='portfolio'>
+            <div className='center'>
+                <h2>Portfolio</h2>
+            </div>
+        </section>
+    )
+}
